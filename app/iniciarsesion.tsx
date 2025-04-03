73e09302bd5df9f143f2d0c3c5c2da5daf121fb6
Xavineta <xavineta@MacBook-Air-de-Xavineta.local>
2025-04-03 15:53:10 +0200

    Primer commit

---
+++ app/iniciarsesion.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function IniciarSesion() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Pantalla de Iniciar Sesión</Text>
+    </View>
+  );
+}
